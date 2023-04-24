2e080e870aa1c8a5cc7123cfa2bb8f61c7ef5227
Tarek Mac <Tarek_rl99@hotmail.com>
2023-04-25 01:43:28 +0200

    Now semi possible to drag and drop the plex folder

--- Mac2Server.py
+++ Mac2Server.py
@@ -41,8 +41,6 @@ if len(sys.argv) > 0:
         client.exec_command('mkdir -p plex/movies plex/series')
 
     print("")
-    print("")
-    print("")
     
     # Find directory of local movies
     local_dir = (f'{os.path.dirname(sys.argv[1])}/plex')
@@ -58,7 +56,6 @@ if len(sys.argv) > 0:
     # # Change the remote working directory to ssh_directory/plex
     # sftp.chdir(ssh_directory+'/plex')
     
-    
     if 'movies' in local_dir:
         # Find directory of local movies
         local_movies_dir = (f'{os.path.dirname(sys.argv[1])}/plex/movies')
@@ -67,68 +64,85 @@ if len(sys.argv) > 0:
         os.chdir(local_movies_dir)
         
         # List up local movies
-        local_movies = os.listdir(local_movies_dir)[1:]
+        local_movies = [file for file in os.listdir(local_movies_dir) if not file.startswith('.')]
+
         
         # Change the remote working directory to ssh_directory/plex/movies
         sftp.chdir(ssh_directory+'/plex/movies')
         
         _, stdout, _ = client.exec_command('ls plex/movies/')
         remote_movies = stdout.read().decode("utf8").split("\n")
-        print(local_movies)
         for local_movie in local_movies:
             local_movie_path = os.path.realpath(local_movie)
             
             # Get the size of the file in bytes
             size = (int(os.path.getsize((local_movie_path))/1024)/1024)/1024
-            
+            print("="*70)
             if local_movie in remote_movies:
-                print(f'Movie already here: {local_movie} ~~~ ({size:.03f}gb)')
+                print(f'||{size:.03f}gb|| ~~~ Movie already here: {local_movie}')
             else:
                 remote_path = f'{ssh_directory}/plex/movies/{local_movie}'
-                print(f'Transfering movie: {local_movie} ~~~ ({size:.03f}gb)')
-                sftp.put(local_movie_path, remote_path)
+                print(f'||{size:.03f}gb|| ~~~ Transfering movie: {local_movie}')
+                # sftp.put(local_movie_path, remote_path)
             
+    if 'series' in local_dir:
+        # Find directory of local movies
+        local_series_dir = (f'{os.path.dirname(sys.argv[1])}/plex/series')
         
-    if 'series' in loc:
-        file_name = os.path.basename(sys.argv[1]).replace(" ", "_").replace("[", "\[").replace("]", "›")
+        # Switch to local movies directory
+        os.chdir(local_series_dir)
+        
+        # List up local movies
+        local_series = ([file for file in os.listdir(local_series_dir) if not file.startswith('.')])
+        
         
         # Change the remote working directory to ssh_directory/plex/series
         sftp.chdir(ssh_directory+'/plex/series')
         
-        _, stdout, _ = client.exec_command('ls plex/series/') 
-        series = stdout.read().decode("utf8").split("\n")
-        
-        if file_name not in series:
-            remote_path = f'{ssh_directory}/plex/series/{file_name}'
-            
-            # Create series folder
-            client.exec_command(f'mkdir plex/series/{file_name}')
+        _, stdout, _ = client.exec_command('ls plex/series/')
+        remote_series = stdout.read().decode("utf8").split("\n")
+        for local_serie in local_series:
+
+            # Save remote path
+            remote_path = f'\'{ssh_directory}/plex/series/{local_serie}\''
             
             # Change the remote working directory to ssh_directory/plex/series/(series folder name)
-            sftp.chdir(f'{ssh_directory}/plex/series')
+            sftp.chdir(f'{ssh_directory}/plex/series/')
             
-            # Go into file_name directory on local computer and sftp all the files there
-            os.chdir(f'{os.path.dirname(sys.argv[1])}/{file_name}')
+            # Create series folder
+            client.exec_command(f'mkdir {remote_path}')
+            
+            # Go into file_name directory on local computer
+            series_folder = f'{os.path.dirname(sys.argv[1])}/plex/series/{local_serie}'
+            os.chdir(series_folder)
             
             # Get a list of all the files in the directory
-            files = os.listdir()
+            episode_list = os.listdir(series_folder)
             
-            for file in files:
-                local_path = f'{os.path.dirname(sys.argv[1])}/{file_name.replace("_", " ")}/{file}'
-                remote_path = f'{ssh_directory}/plex/series/{file_name}/{file}'
+            local_serie_path = os.path.join(local_series_dir, local_serie)
+            
+            # Get size of series folder in GB
+            size = (((os.path.getsize(f'{local_serie_path}')/1024)/1024)/1024)
+            
+            print("="*70)
+            print(f'||{size:.03f}gb|| ~~~ Transerfering Series: {local_serie}')
+            
+            for episode in episode_list:
+                local_path = f'{os.path.dirname(sys.argv[1])}/{local_serie}/{local_serie}'
+                remote_path = f'{ssh_directory}/plex/series/{local_serie}/{local_serie}'
                 
                 # Change the remote working directory to ssh_directory/plex/series/(series name)
-                sftp.chdir(f'{ssh_directory}/plex/series/{file_name}') 
-                 
-                size = ((os.path.getsize(local_path)/1024)/1024)/1024
-                print(f'Transerfering Episode: {file} ~~~ ({size:.03f}gb)') 
+                sftp.chdir(f'{ssh_directory}/plex/series/{local_serie}') 
+                # Calcualte size of file
+                size = (((os.path.getsize(episode)/1024)/1024)/1024)
+                print(f'||{size:.03f}gb|| ~~ Transerfering Episode: {episode}') 
                 # sftp.put(local_path, remote_path)
         else:
-            print(f'Series already here: {file_name} ~~~ ({size:.03f}gb)')
+            print("="*70)
+            print(f'||{size:.03f}gb|| ~~~ Series already here: {local_serie}')
 
-
+    print("")
     sftp.close()
-
     client.close()
     
 else:
